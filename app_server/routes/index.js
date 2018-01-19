d3479a073dcbf313bab9a1725c0389a0fee13ce1
antdear <antonius.camara@gmail.com>
2018-01-19 15:08:25 +0200

    updates

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 // Declaring the controllers
-var ctrlMain = require('../controllers/ctrlMain'); // Controller for the Home page
-var ctrlFootball = require('../controllers/ctrlFootball'); // ... for the list of World Cup winners (countries)
-var ctrlGoldenBall = require('../controllers/ctrlGoldenBall'); // ... for the the list of Golden ball winners (players)
+const ctrlMain = require('../controllers/ctrlMain'); // Controller for the Home page
+const ctrlFootball = require('../controllers/ctrlFootball'); // ... for the list of World Cup winners (countries)
+const ctrlGoldenBall = require('../controllers/ctrlGoldenBall'); // ... for the the list of Golden ball winners (players)
 
 // Defining the routes and associating the routes to their corresponding controllers
 router.get('/', ctrlMain.index);  // Home page
